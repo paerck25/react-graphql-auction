e536da1bdeb3e685e6e1ab13260663423e2904e9
kdj <rlaehdwp7451@gmail.com>
2020-09-12 21:41:42 +0900

    add regExp in join

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -36,13 +36,19 @@ const useStyles = makeStyles((theme) => ({
   button: {
     color: 'rgb(104,104,106)',
     padding: 0,
+  },
+  smallText : {
+    color : 'red',
   }
 }));
 
 const JoinForm = ({ handleLogin }) => {
 
   const classes = useStyles();
-  const history = useHistory();
+
+  const [nameChecked, setNameChecked] = useState(true);
+  const [emailChecked, setEmailChecked] = useState(true);
+  const [pwdChecked, setPwdChecked] = useState(true);
 
   const [signUp, { data }] = useMutation(SIGNUP);
 
@@ -70,11 +76,41 @@ const JoinForm = ({ handleLogin }) => {
     })
   }
 
+  const {name,email,pwd} = userInfo;
+
+  const checkName = () => {
+    const reg = /^[가-힣]{2,4}$/.test(name);
+    if(reg){
+      setNameChecked(true);
+    } else {
+      setNameChecked(false);
+    }
+  }
+
+  const checkEmail = () => {
+    const reg = /^[a-z0-9_+.-]+@([a-z0-9-]+\.)+[a-z0-9]{2,4}$/.test(email);
+    if(reg){
+      setEmailChecked(true);
+    } else {
+      setEmailChecked(false);
+    }
+  }
+
+  const checkPwd = () => {
+    const reg = /[A-Za-z0-9!@#$%^&*()-_+=]{8,}$/.test(pwd);
+    if(reg){
+      setPwdChecked(true);
+    } else {
+      setPwdChecked(false);
+    }
+  }
+
   const onSubmitForm = (e) => {
     e.preventDefault();
-    if (userInfo.name === '' || userInfo.email === '' || userInfo.pwd === '') {
-      alert('빈 항목이 존재합니다.')
-    } else {
+    checkName();
+    checkEmail();
+    checkPwd();
+    if (nameChecked && emailChecked && pwdChecked) {
       signUp({
         variables: {
           input: {
@@ -108,8 +144,11 @@ const JoinForm = ({ handleLogin }) => {
               id="name"
               label="이름"
               autoFocus
+              autoComplete="sex"
               onChange={onChangeInfo}
+              onBlur={checkName}
             />
+            {!nameChecked && <small className={classes.smallText}>2~4 글자인 한글로 작성해 주세요.</small>}
           </Grid>
           <Grid item xs={12}>
             <TextField
@@ -122,7 +161,9 @@ const JoinForm = ({ handleLogin }) => {
               autoComplete="email"
               type="email"
               onChange={onChangeInfo}
+              onBlur={checkEmail}
             />
+            {!emailChecked && <small className={classes.smallText}>올바른 이메일 형식이 아닙니다.</small>}
           </Grid>
           <Grid item xs={12}>
             <TextField
@@ -135,7 +176,9 @@ const JoinForm = ({ handleLogin }) => {
               id="password"
               autoComplete="current-password"
               onChange={onChangeInfo}
+              onBlur={checkPwd}
             />
+            {!pwdChecked && <small className={classes.smallText}>8자 이상 입력해주세요.</small>}
           </Grid>
         </Grid>
         <Button
